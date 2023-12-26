4d781407907a2dc41ba380da6482788b919373e1
michaelhhl <35296890+michaelhhl@users.noreply.github.com>
2023-12-26 14:32:46 +0800

    Update route.ts
    Update route.ts

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,16 +1,6 @@
-import { promises as fs } from 'fs';
 import { Locale } from '@/i18n-config'
 
-export async function getWorkHistory(lang: Locale) {
-    const file = await fs.readFile(process.cwd() + `/app/api/mydata/${lang}/workhistory.json`, 'utf8');
-    return file
-}
-export async function getPersonInfo(lang: Locale) {
-    const file = await fs.readFile(process.cwd() + `/app/api/mydata/${lang}/personinfo.json`, 'utf8');
-    return file
-}
-export async function getProjectExperience(lang: Locale) {
-    const file = await fs.readFile(process.cwd() + `/app/api/mydata/${lang}/projectexperience.json`, 'utf8');
-    return file
-}
 
+export async function GET(request: Request, { params }: { params: { lang: Locale } }) {
+  return Response.json({  })
+}
