f278023cd86d15c7be24a3c3b805a86b744ff78d
michaelhhl <35296890+michaelhhl@users.noreply.github.com>
2023-12-27 13:32:27 +0800

    Update page.tsx
    Update page.tsx

--- app/[lang]/vue/page.tsx
+++ app/[lang]/vue/page.tsx
@@ -15,6 +15,7 @@ export default async function WorkHistoryPage({
     params: { lang: Locale }
 }>) {
 
+    const title = lang === 'cn' ? '管理系统' : '管理体制'
     return (
         <Container>
             <Box
@@ -26,7 +27,7 @@ export default async function WorkHistoryPage({
                 }}
             >
                 <ImageContentCard key='adminsystemlogin'
-                    heading='管理系统'
+                    heading={title}
                     text=''
                     imgUrl={adminsystemlogin}
                     width={900}
@@ -43,25 +44,54 @@ export default async function WorkHistoryPage({
                     imgUrl={adminsystemrole}
                     width={900}
                 />
-                <Typography variant="h6" gutterBottom>
-                    1.此前后端分离项目基于Vue + Spring(Java) + PostgreSQL实现
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                    2.封装Axios，实现Http请求/响应拦截，自定义get，post请求方法
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                    3.通过自定义router，实现菜单功能
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                    4.基于SpringBoot + Mybatis实现前端调用的Api
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                    5.基于SpringSecurity + oauth2 + JWT实现用户认证
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                    6.拦截Api的请求响应，统一响应数据的格式
-                </Typography>
-
+                {
+                    (lang === 'cn') && (
+                        <>
+                            <Typography variant="h6" gutterBottom>
+                                1.此前后端分离项目基于Vue + Spring(Java) + PostgreSQL实现
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                2.封装Axios，实现Http请求/响应拦截，自定义get，post请求方法
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                3.通过自定义router，实现菜单功能
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                4.基于SpringBoot + Mybatis实现前端调用的Api
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                5.基于SpringSecurity + oauth2 + JWT实现用户认证
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                6.拦截Api的请求响应，统一响应数据的格式
+                            </Typography>
+                        </>
+                    )
+                }
+                {
+                    (lang === 'jp') && (
+                        <>
+                            <Typography variant="h6" gutterBottom>
+                                1.Vue＋Spring（Java）＋PostgreSQLの実装によるフロントエンドとバックエンドの分離プロジェクト
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                2.パッケージングAxios、Httpリクエスト/レスポンスインターセプションの実装、カスタムget、postリクエストメソッド
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                3.カスタムルーターを通して、メニュー機能を実現する
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                4.SpringBoot＋Mybatisをベースに、フロントエンドのコールAPIを実現。
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                5.SpringSecurity＋oauth2＋JWTによるユーザー認証
+                            </Typography>
+                            <Typography variant="h6" gutterBottom>
+                                6.APIのリクエスト・レスポンスをインターセプトし、レスポンスのデータ・フォーマットを統一する。
+                            </Typography>
+                        </>
+                    )
+                }
             </Box>
         </Container>
     );
