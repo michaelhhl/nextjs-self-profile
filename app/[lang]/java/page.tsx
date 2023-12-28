69cba823a7ebc078fe42d970548a6a96e046387c
michaelhhl <35296890+michaelhhl@users.noreply.github.com>
2023-12-28 14:06:24 +0800

    Update java page.tsx
    Update java page.tsx

--- app/[lang]/java/page.tsx
+++ app/[lang]/java/page.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-// import ContentCard from '../components/ContentCard';
+import ContentCard from '../components/ContentCard';
 import { Locale } from '@/i18n-config'
+import { getJavaSolution } from '@/utils/jsonHelper';
+import Typography from '@mui/material/Typography';
 
 
 export default async function WorkHistoryPage({
@@ -10,6 +12,9 @@ export default async function WorkHistoryPage({
 }: Readonly<{
     params: { lang: Locale }
 }>) {
+
+    const title = lang === 'cn' ? '场景&解决案' : 'シナリオと解決策'
+    const solutionList = await getJavaSolution(lang)
 
     return (
         <Container>
@@ -21,16 +26,15 @@ export default async function WorkHistoryPage({
                     alignItems: 'center',
                 }}
             >
-                {/* <Typography variant="body1" gutterBottom>
-                    {data}
-                </Typography> */}
-                {/* {experienceList.map((item: any) => {
-                    return (<ContentCard key={item.title}
-                        heading={item.title}
-                        text={item.responsibility}
+                <Typography variant="h5" gutterBottom>
+                    {title}
+                </Typography>
+                {solutionList.map((item: any) => {
+                    return (<ContentCard key={item.question}
+                        heading={item.question}
+                        text={item.solution}
                     />)
-                })} */}
-                building...
+                })}
             </Box>
         </Container>
     );
